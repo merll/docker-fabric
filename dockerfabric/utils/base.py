4909087ba540c6289197c636e4b0b5346ad81aa9
Matthias Erll <matthias@precisdigital.com>
2015-01-09 18:56:16 +0100

    Utility function for looking up addresses.

--- dockerfabric/utils/base.py
+++ dockerfabric/utils/base.py
@@ -21,3 +21,15 @@ def get_current_roles():
     if roledefs:
         return [role for role, hosts in six.iteritems(roledefs) if current_host in hosts]
     return []
+
+
+def get_role_addresses(role_name, interface_name):
+    roledefs = env.get('roledefs')
+    clients = env.get('docker_clients')
+    if roledefs and clients:
+        role_hosts = roledefs.get(role_name)
+        if role_hosts:
+            return set(client_config.interfaces[interface_name]
+                       for client_name, client_config in six.iteritems(clients)
+                       if client_config.get('fabric_host') in role_hosts)
+    return set()
