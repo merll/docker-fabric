e6d6f66cb3cafc82858e66d296e4d0cbf139a54d
Matthias Erll <matthias@erll.de>
2015-06-06 22:03:50 +0200

    Added script actions from Docker-Map.

--- dockerfabric/actions.py
+++ dockerfabric/actions.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from fabric.api import task
+import os
+import posixpath
+
+from fabric.api import get, put, puts, task
+from fabric.utils import error
+import six
 
 from .apiclient import container_fabric
+from .utils.files import temp_dir
 
 
 @task
@@ -122,3 +128,61 @@ def update(map_name, config_name, instances=None, **kwargs):
     :param kwargs: Keyword arguments to the action implementation.
     """
     container_fabric().update(config_name, instances=instances, map_name=map_name, **kwargs)
+
+
+@task
+def script(map_name, config_name, script_path, fail_nonzero=False, upload_dir=False, **kwargs):
+    """
+    Runs a script inside a container, which is created with all its dependencies. The container is removed after it
+    has been run, whereas the dependencies are not destroyed. The output is printed to the console.
+
+    :param map_name: Container map name.
+    :param config_name: Container configuration name.
+    :param script_path: Local path to the script file.
+    :param fail_nonzero: Fail if the script returns with a nonzero exit code.
+    :param upload_dir: Upload the entire parent directory of the script file to the remote.
+    :param kwargs: Additional keyword arguments to the run_script action.
+    """
+    full_script_path = os.path.abspath(script_path)
+    prefix, name = os.path.split(full_script_path)
+    with temp_dir() as remote_tmp:
+        if upload_dir:
+            prefix_path, prefix_name = os.path.split(prefix)
+            remote_script = posixpath.join(remote_tmp, prefix_name, name)
+            put(prefix, remote_tmp, mirror_local_mode=True)
+        else:
+            remote_script = posixpath.join(remote_tmp, name)
+            put(script_path, remote_script, mirror_local_mode=True)
+        results = container_fabric().run_script(config_name, map_name=map_name, script_path=remote_script, **kwargs)
+        for client, res in six.iteritems(results):
+            puts("Exit code: {0}".format(res['exit_code']))
+            if res['exit_code'] == 0 or not fail_nonzero:
+                puts(res['log'])
+            else:
+                error(res['log'])
+
+
+@task
+def single_cmd(map_name, config_name, command, fail_nonzero=False, download_result=None, **kwargs):
+    """
+    Runs a script inside a container, which is created with all its dependencies. The container is removed after it
+    has been run, whereas the dependencies are not destroyed. The output is printed to the console.
+
+    :param map_name: Container map name.
+    :param config_name: Container configuration name.
+    :param fail_nonzero: Fail if the script returns with a nonzero exit code.
+    :param download_result: Download any results that the command has written back to a temporary directory.
+    :param kwargs: Additional keyword arguments to the run_script action.
+    """
+    with temp_dir() as remote_tmp:
+        if 'command_format' not in kwargs:
+            kwargs['command_format'] = ['-c', command]
+        results = container_fabric().run_script(config_name, map_name=map_name, script_path=remote_tmp, **kwargs)
+        for client, res in six.iteritems(results):
+            puts("Exit code: {0}".format(res['exit_code']))
+            if res['exit_code'] == 0 or not fail_nonzero:
+                puts(res['log'])
+            else:
+                error(res['log'])
+            if download_result:
+                get(posixpath.join(remote_tmp, '*'), local_path=download_result)
